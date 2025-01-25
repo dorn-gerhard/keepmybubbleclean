a745be3ec3eadeb92310bd881b46d556594a3b17
Gerhard Dorn <quantum.quentin@mailbox.org>
2025-01-25 15:11:14 +0100

    update camera

--- Assets/Scripts/Spawnmanager.cs
+++ Assets/Scripts/Spawnmanager.cs
@@ -6,17 +6,39 @@ public class Spawnmanager : MonoBehaviour
 {
 
     public float spawnRate;
-    public GameObject bubble; // gameobject without values
+    public Bubble bubble; // gameobject without values
     public List<BubbleScriptableObject> bubbleContent;
+    public bool spawnNow;
+
+    public RectTransform spawnArea;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnArea = this.GetComponent<RectTransform>();
+    }
+    private void OnValidate()
+    {
+        if (spawnNow)
+        {
+            Spawn();
+            spawnNow = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Spawn()
     {
-        
+        // choose new position
+        float xPos = Random.Range(spawnArea.offsetMin.x, spawnArea.offsetMax.x);
+        float yPos = Random.Range(spawnArea.offsetMin.y, spawnArea.offsetMax.y);
+        Debug.Log($"spawnArea: {spawnArea.offsetMin.x}");
+       
+        Bubble newBubble =  Instantiate(bubble,new Vector2(xPos,yPos), new Quaternion());
+
+        // Assign data:
+        var bubbleCont = bubbleContent[0];
+        bubbleContent.RemoveAt(0);
+        newBubble.bubbleData = bubbleCont;
+
+
     }
 }
