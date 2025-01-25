e8c191511c0d9e28be1bd294bc0abd5277375070
Oskar Bechtold <bechtoldster@gmail.com>
2025-01-25 12:45:30 +0100

    Created ScoreDisplay Prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float anxiety = 0;
+    public float distrust = 0;
+    public float fomo = 0;
+    public float loneliness = 0;
+    public float doomScrolling = 0;
+
     // Start is called before the first frame update
     void Start()
     {
