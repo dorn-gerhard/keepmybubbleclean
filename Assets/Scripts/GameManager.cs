c92829b6022c14ac8d9a274ffd40c2e67fe35d54
Oskar Bechtold <bechtoldster@gmail.com>
2025-01-25 13:26:45 +0100

    Linked spike positions to game manager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,12 @@ public class GameManager : MonoBehaviour
     public float loneliness = 0;
     public float doomScrolling = 0;
 
+    public GameObject anxietySpike;
+    public GameObject distrustSpike;
+    public GameObject fomoSpike;
+    public GameObject lonelinessSpike;
+    public GameObject doomScrollingSpike;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +25,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        anxietySpike.transform.localPosition = new Vector3(anxietySpike.transform.localPosition.x, anxiety, anxietySpike.transform.localPosition.z);
+        distrustSpike.transform.localPosition = new Vector3(distrustSpike.transform.localPosition.x, distrust, distrustSpike.transform.localPosition.z);
+        fomoSpike.transform.localPosition = new Vector3(fomoSpike.transform.localPosition.x, fomo, fomoSpike.transform.localPosition.z);
+        lonelinessSpike.transform.localPosition = new Vector3(lonelinessSpike.transform.localPosition.x, loneliness, lonelinessSpike.transform.localPosition.z);
+        doomScrollingSpike.transform.localPosition = new Vector3(doomScrollingSpike.transform.localPosition.x, doomScrolling, doomScrollingSpike.transform.localPosition.z);
     }
 }
