fbd5ae5339988f318d505ed93d489a34a5137555
Oskar Bechtold <bechtoldster@gmail.com>
2025-01-26 14:02:01 +0100

    Mapped the GameManager variables to the audio pramaters.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -10,10 +10,10 @@ public class AudioManager : MonoBehaviour
     private EventInstance instance;
     [SerializeField] EventReference BGTheme;
 
-    [SerializeField][Range(0f, 1f)] public float fomo;
-    [SerializeField][Range(0f, 1f)] public float distrust;
-    [SerializeField][Range(0f, 1f)] public float anxiety;
-    [SerializeField][Range(0f, 1f)] public float loneliness;
+    [SerializeField][Range(0f, 100f)] private float fomo;
+    [SerializeField][Range(0f, 100f)] private float distrust;
+    [SerializeField][Range(0f, 100f)] private float anxiety;
+    [SerializeField][Range(0f, 100f)] private float loneliness;
 
     public void PlayBGTheme()
     {
@@ -31,9 +31,14 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        instance.setParameterByName("FOMO", fomo * 100);
-        instance.setParameterByName("Disstrust", distrust * 100);
-        instance.setParameterByName("Loneliness", anxiety * 100);
-        instance.setParameterByName("Anxiety", loneliness * 100);
+        anxiety = Mathf.Clamp(GameManager.Instance.anxiety, 0, 1) * 100;
+        distrust = Mathf.Clamp(GameManager.Instance.distrust, 0, 1) * 100;
+        loneliness = Mathf.Clamp(GameManager.Instance.loneliness, 0, 1) * 100;
+        fomo = Mathf.Clamp(GameManager.Instance.fomo, 0, 1) * 100;
+    
+        instance.setParameterByName("FOMO", fomo);
+        instance.setParameterByName("Disstrust", distrust);
+        instance.setParameterByName("Loneliness", anxiety);
+        instance.setParameterByName("Anxiety", loneliness);
     }
 }
