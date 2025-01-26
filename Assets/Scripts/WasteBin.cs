e3104e0f22f0d033da626e57f91fb69f10a1ad1a
Oskar Bechtold <bechtoldster@gmail.com>
2025-01-26 12:00:25 +0100

    TrashBat is eating.

--- Assets/Scripts/WasteBin.cs
+++ Assets/Scripts/WasteBin.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class WasteBin : MonoBehaviour
 {
+    public Sprite spriteOpen;
+    public Sprite spriteClosed;
+    public SpriteRenderer spriteRenderer;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,10 +15,19 @@ public class WasteBin : MonoBehaviour
         if (bubble && (bubble.state == BubbleState.FLOATING || bubble.state == BubbleState.STICKY) && bubble.isDragged)
         {
             Debug.Log("Info bubble has hit wast bin");
+            spriteRenderer.sprite = spriteClosed;
+
             Destroy(collision.gameObject);
+
+            StartCoroutine(EatingProcess(0.5f));
         }
     }
 
+    IEnumerator EatingProcess(float washingTime)
+    {
+        yield return new WaitForSeconds(washingTime);
+        spriteRenderer.sprite = spriteOpen;
+    }
     // Start is called before the first frame update
     void Start()
     {
